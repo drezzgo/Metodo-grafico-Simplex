a40ce7600d9ec076827e57742a08502f8d757a78
Andres Gongora <agongoraramirez@gmail.com>
2024-04-14 18:24:09 -0500

    Agregar frame de resultados de metodo grafico y logica de input para restricciones

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 from librerias import *
 
-def inspeccionar_datos(inicio, grafico, input_metodo, input_variables, input_restricciones):
+def inspeccionar_datos(inicio, grafico, simplex, input_metodo, input_variables, input_restricciones):
     metodo = input_metodo.get()
     variables = input_variables.get()
     restricciones = input_restricciones.get()
@@ -34,7 +34,37 @@ def inspeccionar_datos(inicio, grafico, input_metodo, input_variables, input_res
             inicio.grid_remove()
             grafico.grid()
 
+            # Crear entradas para cada restricción
+            for i in range(1, restricciones + 1):
+                # Crear Label e Input para cada restricción
+                texto_restriccion = ttk.Label(grafico, text=f"Restricción {i}:")
+                input_x1_restriccion = ttk.Entry(grafico)
+                texto_ecuacion = ttk.Label(grafico, text=" X1 + ")
+                input_x2_restriccion = ttk.Entry(grafico)
+                texto_ecuacion2 = ttk.Label(grafico, text=" X2 ")
+                input_equivalencia_restriccion = ttk.Combobox(
+                    grafico,
+                    state="readonly",
+                    values=["<=", ">=", "=="]
+                )
+                input_resultado_restriccion = ttk.Entry(grafico)
+
+                # Ubicar los elementos en la misma fila
+                texto_restriccion.grid(row=i + 2, column=0)
+                input_x1_restriccion.grid(row=i + 2, column=1)
+                texto_ecuacion.grid(row=i + 2, column=2)
+                input_x2_restriccion.grid(row=i + 2, column=3)
+                texto_ecuacion2.grid(row=i + 2, column=4)
+                input_equivalencia_restriccion.grid(row=i + 2, column=5)
+                input_resultado_restriccion.grid(row=i + 2, column=6, padx=6)
+
         
         #Si llega aqui es porque escogio simplex y las variables son mayor a 0
         else:
             print("Siguiente parte del Simplex")
+            inicio.grid_remove()
+            simplex.grid()
+
+def volver(ventana_actual, ventana_volver):
+    ventana_actual.grid_remove()
+    ventana_volver.grid()
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ texto_variables = ttk.Label(inicio, text="¿Cuántas variables de decisión tien
 input_variables = ttk.Entry(inicio, )
 texto_restricciones = ttk.Label(inicio, text="¿Cuantas restricciones tiene el problema?")
 input_restricciones = ttk.Entry(inicio, )
-boton_continuar = ttk.Button(inicio, text="Continuar", command = lambda: func.inspeccionar_datos(inicio, grafico, input_metodo, input_variables, input_restricciones))
+boton_continuar = ttk.Button(inicio, text="Continuar", command = lambda: func.inspeccionar_datos(inicio, grafico, simplex, input_metodo, input_variables, input_restricciones))
 
 input_metodo.set("Grafico")
 input_variables.insert(0, "2")
@@ -36,15 +36,53 @@ grafico = ttk.Frame(ventana)
 grafico.grid(row=0, column=0, padx=10, pady=10)
 grafico.grid_remove()
 
-texto_objetivo = ttk.Label(grafico, text="¿Cual es el objetivo de la funcion?")
-input_objetivo = ttk.Combobox(
+texto_grafico = ttk.Label(grafico, text="Grafico")
+texto_objetivo_grafico = ttk.Label(grafico, text="¿Cual es el objetivo de la funcion?")
+input_objetivo_grafico = ttk.Combobox(
                             grafico,
+                            state="readonly",
+                            values=["Maximizar", "Minimizar"])
+texto_funcion_grafico = ttk.Label(grafico, text="Funcion ")
+input_x1_grafico = ttk.Entry(grafico)
+texto_funcion2_grafico = ttk.Label(grafico, text=" X1 + ")
+input_x2_grafico = ttk.Entry(grafico)
+texto_funcion3_grafico = ttk.Label(grafico, text=" X2")
+boton_continuar_grafico = ttk.Button(grafico, text="Evaluar", command= lambda: func.volver(grafico, resultado_grafico))
+boton_volver_grafico = ttk.Button(grafico, text="Volver", command= lambda: func.volver(grafico, inicio))
+
+texto_grafico.grid(row=0, column=0, columnspan=7, padx=10, pady=10 )
+texto_objetivo_grafico.grid(row=1, column=0, columnspan=4, pady=10)
+input_objetivo_grafico.grid(row=1, column=4, columnspan=3)
+boton_volver_grafico.grid(row=10, column=0, columnspan=3, padx=10, pady=10, ipadx=10, ipady=10)
+boton_continuar_grafico.grid(row=10, column=4, columnspan=4, pady=10, ipadx=10, ipady=10)
+
+
+resultado_grafico = ttk.Frame(ventana)
+resultado_grafico.grid(row=0, column=0, padx=10, pady=10)
+resultado_grafico.grid_remove()
+
+texto_resultado_grafico = ttk.Label(resultado_grafico, text="Resultado Grafico")
+boton_volver_grafico = ttk.Button(resultado_grafico, text="Volver", command= lambda: func.volver(resultado_grafico, grafico))
+
+texto_resultado_grafico.grid(row=0, column=0, columnspan=7, padx=10, pady=10 )
+boton_volver_grafico.grid(row=10, column=0, columnspan=7, padx=10, pady=10, ipadx=10, ipady=10)
+
+simplex = ttk.Frame(ventana)
+simplex.grid(row=0, column=0, padx=10, pady=10)
+simplex.grid_remove()
+
+texto_simplex = ttk.Label(simplex, text="Simplex")
+texto_objetivo_simplex = ttk.Label(simplex, text="¿Cual es el objetivo de la funcion?")
+input_objetivo_simplex = ttk.Combobox(
+                            simplex,
                             state="readonly",
                             values=["Maximizar", "Minimizar"]
                         )
+boton_volver_simplex = ttk.Button(simplex, text="Volver", command= lambda: func.volver(simplex, inicio))
 
-texto_objetivo.grid(row=0, column=0)
-input_objetivo.grid(row=0, column=1)
+texto_simplex.grid(row=0, column=0, columnspan=2 )
+texto_objetivo_simplex.grid(row=1, column=0)
+input_objetivo_simplex.grid(row=1, column=1)
+boton_volver_simplex.grid(row=10, column=0, columnspan=2)
 
 ventana.mainloop()
-
